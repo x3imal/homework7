8f9baf3c30a77c133e023d6723699ece3d6f1f73
alexeyasurov <aasurov@gmail.com>
2023-02-22 18:29:14 +0300

    1 task completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+            task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+
+        int accumulation = 15_000;
+        int total = 0;
+        int mans = 0;
+        while (total < 2_459_000) {
+            total = total + accumulation;
+            mans = total / accumulation;
+            System.out.println("Месяц " + mans + " сумма накоплений " + total + " рублей");
+        }
+        //System.out.println("За " + mans + " месяцев, можно накопить 2 459 000 рублей");
     }
 }
